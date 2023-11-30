ef6a7fee1e2cbf9f68874e84eeb1f55a309b5c1c
aighinye <aighinye@gmail.com>
2023-11-30 06:46:46 -0800

    Add the apikey and apiurl

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
+function searchCity(city) {
+    let apiKey = "b2a5adcct04b33178oc335f405433";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${ApiKey}&units=metric';
+console.log(apiUrl);
+}
+
+
+
 function handleSearchSumbmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+searchCity(searchInput.value);
 } 
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSumbmit);
-
-
-
-
