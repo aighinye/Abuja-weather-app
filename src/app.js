fac3de0f6e9bdebb34e8d4da0f28bb223dba6afc
aighinye <aighinye@gmail.com>
2023-11-30 10:49:42 -0800

    Updated the javascript

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
+function refreshWeather(response) {
+   let temperatureElement = document.querySelector("#temperature"); 
+   let temperature = response.data.temperature.current;
+   let cityElement = document.querySelector("#city");
+
+   cityElement = response.data.city;
+   
+temperatureElement.innerHTML = Math.round(temperature);
+    
+}
 function searchCity(city) {
     let apiKey = "243c458c7335db869e116o40t5bf4ad2";
-    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${ApiKey}&units=metric';
-console.log(apiUrl);
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
 }
 
 
@@ -9,8 +19,7 @@ console.log(apiUrl);
 function handleSearchSumbmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+   
 searchCity(searchInput.value);
 } 
 
