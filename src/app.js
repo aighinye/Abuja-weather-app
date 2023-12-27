61ba646244a6d2cb890b12b6e079db581b0ef2a1
aighinye <aighinye@gmail.com>
2023-12-26 23:01:06 -0800

    Updated the HTML,CSS and JAVA Script

--- src/app.js
+++ src/app.js
@@ -1,98 +1,104 @@
-function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windSpeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
-    let iconElement = document.querySelector("#icon");
-  
-    cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML = formatDate(date);
-    descriptionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-    temperatureElement.innerHTML = Math.round(temperature);
-    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-  }
-  
-  function formatDate(date) {
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[date.getDay()];
-  
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-  
-    return `${day} ${hours}:${minutes}`;
-  }
-  
-  function searchCity(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-    axios.get(apiUrl).then(refreshWeather);
-  }
-  
-  function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-  
-    searchCity(searchInput.value);
-  }
+function displayForecast(response) {
+  let forecastHtml = "";
 
-  function displayForecast() {
-    
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-    let forecastHtml = "";
-    days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if (index > 0 && index < 6) {
       forecastHtml =
-      forecastHtml +
-      `
-      <div class="Row">
-      <div class="Col-2">
-        <div class="weather-forecast-day"></div>
-        <div class="weather-forecast-date">
-Tue
-</div>
-<img
-src="https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_2-64.png" 
-alt="" width="42px"
-/>
-<div class="Weather-Forecast-Temperature">
-<span class="Weather-Forecast-Temperature-Max">
-18° 
-</span>
-<span class="Weather-Forecast-Temperature-Min">
-12°
-</span>
-</div>
-      </div>
-    </div>
-      
-      `;
+        forecastHtml +
+        `                
+            <div class="weather-forecast-day">
+                <div class="weather-forecast-date">${formatDay(day.time)}</div>
+                <img src="${
+                  day.condition.icon_url
+                }" class="weather-forecast-icon" />
+                <div class="weather-forecast-temperatures">
+                    <div class="weather-forecast-temperature">
+                        <strong>${Math.round(day.temperature.maximum)}º</strong>
+                    </div>
+                    <div class="weather-forecast-temperature">${Math.round(
+                      day.temperature.minimum
+                    )}º</div>
+                </div>
+            </div>
+        `;
+    }
+  });
 
-    });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
-    let forecastElement = document.querrySelector("#forecast");
-forecastElement.innerHtml = forecastHtml;
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiUrlForecast = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrlForecast).then(displayForecast);
+}
+
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
-  
-  let searchFormElement = document.querySelector("#search-form");
-  searchFormElement.addEventListener("submit", handleSearchSubmit);
-  
-  searchCity("Paris");
-  displayForecast();
 
-  
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let apiKey = "b2a5adcct04b33178913oc335f405433";
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Abuja");
+displayForecast();
