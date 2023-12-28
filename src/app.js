744dec3c041711e3f9ab72e1aab14ecc2a60ff91
aighinye <aighinye@gmail.com>
2023-12-28 13:05:45 -0800

    Updated the JS

--- src/app.js
+++ src/app.js
@@ -8,8 +8,7 @@ function displayForecast(response) {
         `                
             <div class="weather-forecast-day">
                 <div class="weather-forecast-date">${formatDay(day.time)}</div>
-                <img src="${
-                  day.condition.icon_url
+                <img src="${day.condition.icon_url
                 }" class="weather-forecast-icon" />
                 <div class="weather-forecast-temperatures">
                     <div class="weather-forecast-temperature">
@@ -27,6 +26,8 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
+
+
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
